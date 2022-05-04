eb08a1196dad3f2207533e541ce8abd3e0ce4a0c
Welliton Souza <wellitonaz@yahoo.com.br>
2022-05-04 14:42:33 -0300

    Ajustes Finais

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 import { ReactComponent as Dogs } from "../Assets/dogs-footer.svg";
 
 export default function Footer() {
-  return <footer className={styles.footer}>
-    <Dogs />
-    <p>Dog. Alguns direitos reservados.</p>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dog. Alguns direitos reservados.</p>
+    </footer>
+  );
 }
